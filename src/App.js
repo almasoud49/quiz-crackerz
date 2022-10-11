d4398d596781c95d14360f016a98b599b3727d57
Abdullah Al Masoud <almasoud49@yahoo.com>
2022-10-11 20:53:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-2xl font-bold'>This is a big header.</h1>
+      <p className='text-3xl font-bold '>This is just a paragraph.</p>
     </div>
   );
 }
